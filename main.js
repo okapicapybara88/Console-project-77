06e7dbfd7def1e1965b23e3d6a83afe7ca8f11c6
okapicapybara88 <81312147+okapicapybara88@users.noreply.github.com>
2021-08-05 08:33:37 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+ console.log("My first Console project.");
+    var first_name ="Adisenne"
+    var last_name ="Taylor"
+    console.log(first_name + last_name)
+
+    var a = 5;
+    var b = 10;
+
+    var add = a + b
+    console.log(add)
+
+    var sub = a - b
+    console.log(sub)
+
+    var multiple = a * b
+    console.log(multiple)
+
+    var divide = a / b
+    console.log(divide)
